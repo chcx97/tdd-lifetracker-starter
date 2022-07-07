053a8cb9822a2b30f72d31141ecebc1e46050823
chcx97 <xiongchristy@gmail.com>
2022-07-07 09:58:23 -0700

    updated navbar and navlinks

--- lifetracker-ui/src/components/NavLinks/NavLinks.jsx
+++ lifetracker-ui/src/components/NavLinks/NavLinks.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import "./NavLinks.css"
 
-export default function NavLinks() {
+export default function NavLinks(props) {
+  const navigate = useNavigate();
+  console.log(13,props.appState)
+  const logOut = () =>{
+    props.setAppState({})
+    navigate("/login")
+  }
+
   return (
     <div className='navlinks'>
-      <Link className='links' to="/activity">Activity</Link>
-      <Link className='links' to="/nutrition">Nutrition</Link>
+      <span className='nav-header'>
+          {Object.keys(props.appState).length !== 0 ? 
+          <span className='logged-in'>
+            <Link id='activity' className='links' to="/activity">Activity</Link>
+            <Link className='links' to="/nutrition">Nutrition</Link>
+            <button className='sign-out' onClick={logOut}>Sign Out</button>
+          </span>
+          : null}
+          {Object.keys(props.appState).length === 0 ? 
+          <span className='logged-out'>
+            <Link id='activity' className='links' to="/login">Activity</Link>
+            <Link className='links' to="/login">Nutrition</Link>
+            <button className='login' onClick={()=>navigate("/login")} >Login</button>
+            <button className='register' onClick={()=>navigate("/register")}>Sign Up</button>
+          </span>: null}
+      </span>
     </div>
   )
 }
--- lifetracker-ui/src/components/Navbar/Navbar.jsx
+++ lifetracker-ui/src/components/Navbar/Navbar.jsx
@@ -5,12 +5,7 @@ import Logo from "../Logo/Logo"
 import NavLinks from "../NavLinks/NavLinks"
 
 export default function Navbar(props) {
-  const navigate = useNavigate();
-  console.log(13,props.appState)
-  const logOut = () =>{
-    props.setAppState({})
-    navigate("/")
-  }
+
   return (
     <div className='navbar'>
       <div className='header'>
@@ -18,16 +13,7 @@ export default function Navbar(props) {
           <Logo/>
         </div>
         <div className='navlinks-div'>
-          <NavLinks className='navlinks'/>
-        </div>
-        <div className='nav-btns'>
-          {Object.keys(props.appState).length !== 0 ? 
-          <button className='sign-out' onClick={logOut}>Sign Out</button>: null}
-          {Object.keys(props.appState).length === 0 ? 
-          <div>
-            <button className='login' onClick={()=>navigate("/login")} >Login</button>
-            <button className='register' onClick={()=>navigate("/register")}>Register</button>
-          </div>: null}
+          <NavLinks className='navlink' appState={props.appState} setAppState = {props.setAppState}/>
         </div>
       </div>
     </div>
